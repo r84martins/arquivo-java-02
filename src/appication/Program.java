ffc89b71330517a4f6e3d5d78eb8a89d2aac230c
Ricardo Martins <rs84martins@gmail.com>
2020-07-05 22:27:19 -0300

    Implementado boco try-with-resources

--- src/appication/Program.java
+++ src/appication/Program.java
@@ -8,14 +8,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		String path = "c:\\temp\\ina.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		String path = "c:\\temp\\in.txt";
+	
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			while(line != null) {
 				System.out.println(line);
@@ -23,19 +18,6 @@ public class Program {
 			}			
 		}catch(IOException e ) {
 			System.out.println("Error: " + e.getMessage());			
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
